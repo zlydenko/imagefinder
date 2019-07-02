a90f66f0009ff06827423ab76ebcd5baf13dde3a
zlydenko.a <anton.zlydenko@gmail.com>
2019-07-03 01:29:57 +0300

    loading state

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import Navbar from "./components/Navbar";
 import Search from "./components/Search";
 import ImageGrid from "./components/ImageGrid";
 import Pagination from "./components/Pagination";
+import CircularProgress from "@material-ui/core/CircularProgress";
 
 import Axios from "axios";
 
@@ -10,8 +11,10 @@ function App() {
   const [searchValue, setSearchValue] = React.useState("");
   const [searchResults, setSearchResults] = React.useState([]);
   const [imagesFound, setTotalCount] = React.useState(null);
+  const [loading, toggleLoading] = React.useState(false);
 
   const search = (value, pageNumber = null) => {
+    toggleLoading(true);
     let request = {
       username: "vasya",
       input: value
@@ -21,13 +24,15 @@ function App() {
 
     setSearchValue(value);
 
-    Axios.post("http://192.168.0.35:9001/api/search", request).then(
-      ({ data: { data, total } }) => {
+    Axios.post("http://192.168.0.35:9001/api/search", request)
+      .then(({ data: { data, total } }) => {
         if (data.length === 0) setTotalCount(null);
         setSearchResults(data);
         setTotalCount(total);
-      }
-    );
+      })
+      .finally(() => {
+        toggleLoading(false);
+      });
   };
 
   const likeImage = (imageUrl, state) => {
@@ -50,12 +55,16 @@ function App() {
         authenticated={true}
       >
         <Search searchFn={search} />
-        {!!imagesFound && (
-          <ImageGrid
-            images={searchResults}
-            likeFn={likeImage}
-            refetchFn={() => search(searchValue)}
-          />
+        {loading ? (
+          <CircularProgress />
+        ) : (
+          !!imagesFound && (
+            <ImageGrid
+              images={searchResults}
+              likeFn={likeImage}
+              refetchFn={() => search(searchValue)}
+            />
+          )
         )}
       </Navbar>
       {!!imagesFound && (
