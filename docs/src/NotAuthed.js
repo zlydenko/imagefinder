9c42bd659999c5dbdebdc0e80b32abe350c8f126
anton <anton.zlydenko@gmail.com>
2019-07-03 16:04:11 +0300

    ...

--- docs/src/NotAuthed.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import Login from './components/Login';
-
-export default function NotAuthed(props) {
-  return <Login logInFn={props.logInFn} registerFn={props.registerFn} />;
-}
